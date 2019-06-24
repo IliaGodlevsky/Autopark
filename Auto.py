54ab2a6bc6e724d5a8b9b5618b58e2bac27e90dd
IliaGodlevsky <godleevsky2015@gmail.com>
2019-06-24 20:48:59 +0300

    Update Auto.py

--- Auto.py
+++ Auto.py
@@ -37,7 +37,11 @@ class Report:
         self.mileage_till_recovery = mileage_till_recovery
 
     def report(self):
-        pass
+        print("Mileage is: ", self.mileage)
+        print("Cost remained: ", self.cost)
+        print("Spent on fuel: ", self.cost_of_fuel_per_trip)
+        print("Tanked up: ", self.tanked_up)
+        print("Mileage till recovery: ", self.mileage_till_recovery)
 
 
 # base class
@@ -51,8 +55,8 @@ class Car:
         self.car_cost = car_cost
         self.fuel_cost = fuel_cost
         self.__mileage = 0
-        self.__fuel_consumption = 0
-        self.__max_mileage_to_service = 0
+        self.__fuel_consumption = 8.4
+        self.__max_mileage_to_service = 150000
         self.service_cost = service_cost
         self.service_mileage = service_mileage
         self.cost_losing = cost_losing
@@ -64,12 +68,16 @@ class Car:
             self.tank_up()
             self.enhance_car_cost(self.cost_losing)
             self.enhance_fuel_consumption(self.ENHANCE_FUEL_CONSUMPTION)
-            self.service()
+        self.board_computer.set_car_cost(self.car_cost)
+        self.board_computer.set_mileage(self.__mileage)
+        self.board_computer.set_recovery_mileage((self.mileage_till_recovery()))
         return self.__mileage == distance
 
-    def service(self):
-        if self.__mileage % self.__max_mileage_to_service == 0:
-            pass
+    def mileage_till_recovery(self):
+        mileage = self.__mileage
+        while mileage - self.__max_mileage_to_service >= 0:
+            mileage -= self.__max_mileage_to_service
+        return mileage
 
     def enhance_fuel_consumption(self, value):
         if self.__mileage % REDUCE_COST_EACH_INTERVAL == 0:
@@ -85,6 +93,8 @@ class Car:
     def tank_up(self):
         if self.tank == 0:
             self.tank = self.__tank_capacity
+            self.board_computer.tank_up()
+            self.board_computer.count_fuel_cost(self.__tank_capacity * self.fuel_cost)
 
     def report(self):
         self.board_computer.report()
@@ -108,18 +118,41 @@ class DieselCar(Car):
         return not self < other
 
 
-class Factory:
-    def __call__(self, quantity_of_cars):
-        for it in range(0, quantity_of_cars):
-            if it % 3 == it % 5:
-                return DieselCar(BETTER_TANK, CAR_COST, DIESEL_COST_PER_LITRE,
-                                 DIESEL_SERVICE_COST, DIESEL_SERVICE_MILEAGE, DIESEL_COST_LOSING)
-            elif it % 3 == 0:
-                return DieselCar(NORMAL_TANK, CAR_COST, DIESEL_COST_PER_LITRE,
-                                 DIESEL_SERVICE_COST, DIESEL_SERVICE_MILEAGE, DIESEL_COST_LOSING)
-            elif it % 5 == 0:
-                return PetrolCar(BETTER_TANK, CAR_COST, PETROL_COST_PER_LITRE,
-                                 PETROL_SERVICE_COST, PETROL_SERVICE_MILEAGE, PETROL_COST_LOSING)
-            else:
-                return PetrolCar(NORMAL_TANK, CAR_COST, PETROL_COST_PER_LITRE,
-                                 PETROL_SERVICE_COST, PETROL_SERVICE_MILEAGE, PETROL_COST_LOSING)
+# class Factory:
+#     def produce_cars(self, quantity_of_cars=100):
+#         park_of_petrols = []
+#         park_of_diesels = []
+#         auto_park = [park_of_petrols, park_of_diesels]
+#         for it in range(0, quantity_of_cars):
+#             if it % 3 == it % 5:
+#                 auto_park[1].append(DieselCar(BETTER_TANK, CAR_COST, DIESEL_COST_PER_LITRE, DIESEL_SERVICE_COST,DIESEL_SERVICE_MILEAGE, DIESEL_COST_LOSING))
+#             elif it % 3 == 0:
+#                 auto_park[1].append(DieselCar(NORMAL_TANK, CAR_COST, DIESEL_COST_PER_LITRE, DIESEL_SERVICE_COST,DIESEL_SERVICE_MILEAGE, DIESEL_COST_LOSING))
+#             elif it % 5 == 0:
+#                 auto_park[0].append(PetrolCar(BETTER_TANK, CAR_COST, PETROL_COST_PER_LITRE, PETROL_SERVICE_COST,PETROL_SERVICE_MILEAGE, PETROL_COST_LOSING))
+#             else:
+#                 auto_park[0].append(PetrolCar(NORMAL_TANK, CAR_COST, PETROL_COST_PER_LITRE, PETROL_SERVICE_COST,PETROL_SERVICE_MILEAGE, PETROL_COST_LOSING))
+#         return auto_park
+
+
+# factory = Factory()
+# auto_park = factory.produce_cars()
+# for car in auto_park[0]:
+#     car.drive(55000)
+# for car in auto_park[1]:
+#     car.drive(200000)
+
+
+# auto_park[1][0].report()
+
+car = PetrolCar(NORMAL_TANK, CAR_COST, PETROL_COST_PER_LITRE, PETROL_SERVICE_COST,PETROL_SERVICE_MILEAGE,
+                PETROL_COST_LOSING)
+car1 = car
+car.drive(100000)
+car.report()
+
+
+
+
+
+
