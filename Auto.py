b0ed443d2c767306e1f85468bf832a28da95b06b
IliaGodlevsky <godleevsky2015@gmail.com>
2019-06-25 20:09:15 +0300

    Update Auto.py

--- Auto.py
+++ Auto.py
@@ -14,6 +14,8 @@ DIESEL_CAR_COST_LOSING = 10.5
 FUEL_CONSUMPTION_INCREASE = 1.0
 MILEAGE_TILL_GET_OLDER = 1000
 
+STOP = False
+GO = True
 
 class Report:
     pass
@@ -34,5 +36,46 @@ class Car:
         self.__cost_losing = cost_losing
         self.__fuel_cost = fuel_cost
         self.__fuel_consumption = fuel_consumption
+        self.__start_fuel_consumption = fuel_consumption
+        self.__service_cost = service_cost
+        self.__mileage_till_recovery = mileage_till_recovery
+        self.__mileage = 0
+        self.__status = GO
+        self.__board_computer = Report()
+
+    def drive(self, distance):
+        if self.__status == GO:
+            for it in range(0, distance):
+                self.__mileage += 1
+                self.__fuel_consume()
+                self.__tank_up()
+                self.__increase_fuel_consumption()
+                self.__reduce_cost()
+                self.__service()
+
+    def __fuel_consume(self):
+        self.__current_tank_capacity -= self.__fuel_consumption/100.0
+
+    def __increase_fuel_consumption(self):
+        if self.__mileage % MILEAGE_TILL_GET_OLDER == 0:
+            self.__fuel_consumption += self.__start_fuel_consumption * (FUEL_CONSUMPTION_INCREASE/100.0)
+
+    def __reduce_cost(self):
+        if self.__mileage % MILEAGE_TILL_GET_OLDER == 0:
+            self.__car_cost -= self.__cost_losing
+
+    def __out_of_fuel(self):
+        return self.__current_tank_capacity <= 0
+
+    def __tank_up(self):
+        if self.__out_of_fuel():
+            self.__current_tank_capacity=self.__tank_capacity
+
+    def __service(self):
+        if self.__mileage % self.__mileage_till_recovery == 0:
+            self.__status = STOP
+
+
+
 
 
