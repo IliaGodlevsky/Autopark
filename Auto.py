2d1784f9f8503707ace11509c8a5471b751acd4f
IliaGodlevsky <godleevsky2015@gmail.com>
2019-06-25 21:47:32 +0300

    Update Auto.py

--- Auto.py
+++ Auto.py
@@ -40,6 +40,9 @@ class Report:
     def tanked_up(self):
         self.__times_of_tanking_up += 1
 
+    def mileage_to_recovery(self):
+        return self.__mileage_to_recovery
+
     def report(self):
         print("Total mileage: ", self.__mileage)
         print("Cost remained: ", self.__cost_remain)
@@ -67,7 +70,7 @@ class Car:
         self.__service_cost = service_cost
         self.__mileage_till_recovery = mileage_till_recovery
         self.__mileage = 0
-        self.__board_computer = Report()
+        self._board_computer = Report()
 
     def drive(self, distance):
         while self.__mileage < distance:
@@ -77,9 +80,9 @@ class Car:
             self.__increase_fuel_consumption()
             self.__reduce_cost()
             self.__service()
-        self.__board_computer.set_mileage(self.__mileage)
-        self.__board_computer.set_remaining_cost(self._car_cost)
-        self.__board_computer.set_mileage_to_recovery(self.__mileage_till_recovery)
+        self._board_computer.set_mileage(self.__mileage)
+        self._board_computer.set_remaining_cost(self._car_cost)
+        self._board_computer.set_mileage_to_recovery(self.__mileage_till_recovery)
 
     def __fuel_consume(self):
         self.__current_tank_capacity -= self.__fuel_consumption/100.0
@@ -98,15 +101,15 @@ class Car:
     def __tank_up(self):
         if self.__out_of_fuel():
             self.__current_tank_capacity = self.__tank_capacity
-            self.__board_computer.count_fuel_cost(self.__tank_capacity * self.__fuel_cost)
-            self.__board_computer.tanked_up()
+            self._board_computer.count_fuel_cost(self.__tank_capacity * self.__fuel_cost)
+            self._board_computer.tanked_up()
 
     def __service(self):
         if self.__mileage % self.__mileage_till_recovery == 0:
            return True
 
     def report(self):
-        self.__board_computer.report()
+        self._board_computer.report()
 
     def __gt__(self, other):
         pass
@@ -133,15 +136,22 @@ class DieselCar(Car):
         return self._car_cost < other._car_cost
 
 
-car = DieselCar(BETTER_TANK, CAR_COST, DIESEL_CAR_COST_LOSING,
-                DIESEL_COST, DIESEL_FUEL_CONSUMPTION,
-                DIESEL_SERVICE_COST, DIESEL_MILEAGE_TILL_RECOVERY)
-car2 = DieselCar(BETTER_TANK, CAR_COST, PETROL_CAR_COST_LOSING, DIESEL_COST, DIESEL_FUEL_CONSUMPTION,DIESEL_SERVICE_COST, DIESEL_MILEAGE_TILL_RECOVERY)
+class PetrolCar(Car):
+    def __init__(self, tank_capacity,
+                 car_cost,
+                 cost_losing,
+                 fuel_cost,
+                 fuel_consumption,
+                 service_cost,
+                 mileage_till_recovery):
+        super().__init__(tank_capacity, car_cost, cost_losing, fuel_cost, fuel_consumption, service_cost,
+                         mileage_till_recovery)
 
-car.drive(20000)
-car2.drive(20000)
+    def __gt__(self, other):
+        return self._board_computer.mileage_to_recovery() > other._board_computer.mileage_to_recovery()
 
-better = car2 > car
-print(better)
+    def __lt__(self, other):
+        return not self > other
+
 
 
